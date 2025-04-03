215742b9cd0eb3547e7d1f5e19f01a312168afdb
JenW79 <jkwill646@gmail.com>
2025-04-02 20:24:47 -0500

    added backend/frontend folders for clairity, working on redux thunks and components

---
+++ frontend/react-vite/src/components/Reviews/DeleteReviewButton.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+import { useDispatch } from 'react-redux';
+import { removeReview } from "../../redux/reviews";
+
+const DeleteReviewButton = ({ reviewId }) => {
+    const dispatch = useDispatch();
+    
+}
