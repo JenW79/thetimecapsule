d4edbf195c5f38a9347f55d1fae36b6f197da2de
andrea17463 <andrea174633@gmail.com>
2025-04-03 12:21:49 -0700

    added back css for Cart Page component and updated backend/app/__init__.py to match new paths after new backend folder and new frontend folder were created in order to run backend server

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, session, redirect, send_from_directory
+from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
@@ -88,18 +88,15 @@ def react_root(path):
     react builds in the production environment for favicon
     or index.html requests
     """
-    # if path == 'favicon.ico':
-    #     return app.send_from_directory('public', 'favicon.ico')
-    # return app.send_static_file('index.html')
     if path == 'favicon.ico':
-        return send_from_directory(app.static_folder, 'favicon.ico')
-        
+        return app.send_static_file('favicon.ico')
+
     if path and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
+        return app.send_static_file(path)
     
-    return send_from_directory(app.static_folder, 'index.html')
+    return app.send_static_file('index.html')
 
 @app.errorhandler(404)
 def not_found(e):
     # return app.send_static_file('index.html')
-    return send_from_directory(app.static_folder, 'index.html')
+    return app.send_static_file('index.html')
