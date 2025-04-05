96a1e676e42e1195b068af6cb88e4be3ed53b33d
andrea17463 <andrea174633@gmail.com>
2025-04-04 18:35:18 -0700

    revised cart redux

--- frontend/react-vite/src/components/CartPage/AddtoCartButton.jsx
+++ frontend/react-vite/src/components/CartPage/AddtoCartButton.jsx
@@ -1,18 +1,50 @@
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../redux/cart';
+// import { useDispatch } from 'react-redux';
+// import { addToCart } from '../../redux/cart';
 
-const AddtoCartButton = ({ item }) => {
-    const dispatch = useDispatch();
+// const AddtoCartButton = ({ item }) => {
+//     const dispatch = useDispatch();
 
-    const handleAdd = async () => {
-        dispatch(addToCart(item));
+//     const handleAdd = async () => {
+//         dispatch(addToCart(item));
+//     };
+
+//     return (
+//         <button onClick={handleAdd}>
+//             Add to Cart
+//         </button>
+//     );
+// }
+
+// export default AddtoCartButton;
+
+
+
+
+// redux/cart.js
+
+export const addToCart = (item) => {
+    return async (dispatch) => {
+        try {
+            // Call your API to add the item to the cart
+            const response = await fetch('/api/cart', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(item),
+            });
+            
+            if (response.ok) {
+                const data = await response.json();
+                dispatch({
+                    type: 'ADD_TO_CART',
+                    payload: data,
+                });
+            } else {
+                console.error('Error adding to cart');
+            }
+        } catch (error) {
+            console.error('Failed to add to cart:', error);
+        }
     };
-
-    return (
-        <button onClick={handleAdd}>
-            Add to Cart
-        </button>
-    );
-}
-
-export default AddtoCartButton;
+};
