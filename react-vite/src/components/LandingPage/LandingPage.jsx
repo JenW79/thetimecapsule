309a1b48eb5ce68e38989276435f755f5e9c381e
andrea17463 <andrea174633@gmail.com>
2025-04-10 18:03:12 -0700

    changed buttons on Navigation and fixed size of the capsule image

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -4,11 +4,6 @@ function LandingPage() {
     return (
         <div className="landing-page">
             <h1>Welcome to The Time Capsule!</h1>
-            <img
-                src="/the-time-capsule-landing-page.png"
-                alt="The Time Capsule"
-                className="landing-page-image"
-            />
         </div>
     );
 }
