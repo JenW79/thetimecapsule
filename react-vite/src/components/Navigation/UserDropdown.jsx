df9ba0e697b372725b53016a9d06dc33cc85bb64
JenW79 <jkwill646@gmail.com>
2025-04-12 09:16:19 -0500

    added icons to user menu, use npm install react-icons in root

--- react-vite/src/components/Navigation/UserDropdown.jsx
+++ react-vite/src/components/Navigation/UserDropdown.jsx
@@ -1,5 +1,7 @@
 import { useDispatch } from "react-redux";
 import { thunkLogout } from "../../redux/session"
+import { FiUser, FiClipboard, FiMail, FiGift, FiSettings, FiLogOut, FiCreditCard, FiTag } from "react-icons/fi";
+
 import './UserDropDown.css'
 
 export default function UserDropDown( { user, closeMenu }) {
@@ -14,24 +16,25 @@ export default function UserDropDown( { user, closeMenu }) {
     return (
         <div className="user-dropdown">
         <div className="user-dropdown-header">
-          <img className="user-avatar" src="/default-avatar.png" alt="User" />
+        <FiUser className="user-avatar-icon" />
+          {/* <img className="user-avatar" src="/default-avatar.png" alt="User" /> */}
           <div>
-            <strong>{user.username}</strong>
+            <strong className="user-name">{user.username}</strong>
             <p className="user-email">{user.email}</p>
           </div>
         </div>
   
         <div className="user-dropdown-section">
-          <div className="user-dropdown-item">Purchases and reviews</div>
-          <div className="user-dropdown-item">Messages(coming soon)</div>
-          <div className="user-dropdown-item">Credit balance: $0.00(coming soon)</div>
-          <div className="user-dropdown-item">Special offers(coming soon)</div>
-          <div className="user-dropdown-item">Registry(coming soon)</div>
+          <div className="user-dropdown-item"><FiClipboard className="dropdown-icon" />Purchases and reviews</div>
+          <div className="user-dropdown-item"> <FiMail className="dropdown-icon" />Messages(coming soon)</div>
+          <div className="user-dropdown-item"><FiCreditCard className="dropdown-icon" />Credit balance: $0.00(coming soon)</div>
+          <div className="user-dropdown-item"><FiTag className="dropdown-icon" />Special offers(coming soon)</div>
+          <div className="user-dropdown-item"><FiGift className="dropdown-icon" />Registry(coming soon)</div>
         </div>
   
         <div className="user-dropdown-section">
-          <div className="user-dropdown-item">Account settings(coming soon)</div>
-          <div className="user-dropdown-item" onClick={logout}>Sign out</div>
+          <div className="user-dropdown-item"><FiSettings className="dropdown-icon" />Account settings(coming soon)</div>
+          <div className="user-dropdown-item" onClick={logout}><FiLogOut className="dropdown-icon" />Sign out</div>
         </div>
       </div>
     )
