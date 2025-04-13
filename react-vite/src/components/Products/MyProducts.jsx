2f8caeceb064122f439426a57df9e2ecdfe6b78a
JenW79 <jkwill646@gmail.com>
2025-04-13 18:57:56 -0500

    added listings page for current user/css

--- react-vite/src/components/Products/MyProducts.jsx
+++ react-vite/src/components/Products/MyProducts.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCurrentUserProducts } from "../../redux/products";
 import ProductItem from "./ProductItem";
-import "./Products.css";
+import './MyProducts.css'
 
 const MyProducts = () => {
   const dispatch = useDispatch();
@@ -30,7 +30,10 @@ const MyProducts = () => {
 
   return (
     <div className="product-list">
-      <h2>My Listings</h2>
+      <div className="my-products-header">
+  <h2>My Listings</h2>
+  <a href="/products/new" className="cta-button small">+ Create New Listing</a>
+</div>
       <div className="products-grid">
         {products.map((product) => (
           <ProductItem
