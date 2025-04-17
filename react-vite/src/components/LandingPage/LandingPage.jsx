a65969de2c952921ef6df180f483c1b4648b1ee6
andrea17463 <163758451+andrea17463@users.noreply.github.com>
2025-04-17 12:03:17 -0700

    Andrea (#113)

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -22,75 +22,39 @@ function LandingPage() {
 
       <div className="landing-page">
         <h1 className="landing-title">Welcome to The Time Capsule!</h1>
-        <MouseSprinkles /> 
         <div className="nostalgia-container">
-          <div className="nostalgia-item-wrapper">
-            <img
-              src={crescent}
-              alt="Crescent"
-              className="shape shape-crescent"
-            />
-            <div className="nostalgia-item">
-              <img
-                src={eightiesImage}
-                className="circle-img"
-                alt="80s nostalgia"
-              />
-              <div className="nostalgia-info-box">
-                <div className="nostalgia-text-wrapper">
-                  <span>80s nostalgia</span>
-                  <br />
-                  <NavLink
-                    to="/products?decade=80s"
-                    className="shop-now-button"
-                  >
-                    <button>Shop Now</button>
-                  </NavLink>
-                </div>
+          <div className="nostalgia-set">
+            <img src={crescent} alt="Crescent" className="shape shape-crescent" />
+            <img src={eightiesImage} className="circle-img" alt="80s nostalgia" />
+            <div className="nostalgia-info-box">
+              <div className="nostalgia-text-wrapper">
+                <span>80s nostalgia</span>
+                <NavLink to="/products?decade=80s" className="shop-now-button">
+                  <button>Shop Now</button>
+                </NavLink>
               </div>
             </div>
           </div>
 
-          <div className="nostalgia-item-wrapper">
-            <img
-              src={rectangle}
-              alt="Rectangle"
-              className="shape shape-rectangle"
-            />
-            <div className="nostalgia-item">
-              <img
-                src={ninetiesImage}
-                className="circle-img"
-                alt="90s nostalgia"
-              />
-              <div className="nostalgia-info-box">
-                <div className="nostalgia-text-wrapper">
-                  <span>90s nostalgia</span>
-                  <br />
-                  <NavLink
-                    to="/products?decade=90s"
-                    className="shop-now-button"
-                  >
-                    <button>Shop Now</button>
-                  </NavLink>
-                </div>
+          <div className="nostalgia-set">
+            <img src={rectangle} alt="Rectangle" className="shape shape-rectangle" />
+            <img src={ninetiesImage} className="circle-img" alt="90s nostalgia" />
+            <div className="nostalgia-info-box">
+              <div className="nostalgia-text-wrapper">
+                <span>90s nostalgia</span>
+                <NavLink to="/products?decade=90s" className="shop-now-button">
+                  <button>Shop Now</button>
+                </NavLink>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="nostalgia-item-wrapper">
-          <img src={triangle} alt="Triangle" className="shape shape-triangle" />
-          <div className="nostalgia-item">
-            <img
-              src={twoThousandsImage}
-              className="circle-img"
-              alt="00s nostalgia"
-            />
+          <div className="nostalgia-set">
+            <img src={triangle} alt="Triangle" className="shape shape-triangle" />
+            <img src={twoThousandsImage} className="circle-img" alt="00s nostalgia" />
             <div className="nostalgia-info-box">
               <div className="nostalgia-text-wrapper">
                 <span>00s nostalgia</span>
-                <br />
                 <NavLink to="/products?decade=00s" className="shop-now-button">
                   <button>Shop Now</button>
                 </NavLink>
@@ -98,74 +62,46 @@ function LandingPage() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="top-rated-wrapper">
-        <h1 className="top-rated-products">Top Rated Products</h1>
+        <div className="top-rated-wrapper">
+          <h1 className="top-rated-products">Top Rated Products</h1>
 
-        <div className="top-rated-images-container">
-          <div className="top-rated-item">
-            <div className="top-rated-image-wrapper">
-              <img
-                src={periwinkleSprinkles}
-                alt="Periwinkle Sprinkles"
-                className="sprinkle-bg sprinkle-peri"
-              />
-              <img
-                src={topRatedImage1}
-                className="square-img"
-                alt="top rated nostalgia 1"
-              />
+          <div className="top-rated-images-container">
+            <div className="top-rated-item">
+              <div className="top-rated-image-wrapper">
+                <img src={periwinkleSprinkles} alt="Periwinkle Sprinkles" className="sprinkle-bg sprinkle-peri" />
+                <img src={topRatedImage1} className="square-img" alt="top rated nostalgia 1" />
+              </div>
+              <span>red & blue pokemon gameboy games</span>
+              <NavLink to="/products" className="shop-now-button">
+                <button>Shop Now</button>
+              </NavLink>
             </div>
-            <span>red & blue pokemon gameboy games</span>
-            <NavLink to="/products" className="shop-now-button">
-              <button>Shop Now</button>
-            </NavLink>
-          </div>
 
-          <div className="top-rated-item">
-            <div className="top-rated-image-wrapper">
-              <img
-                src={periwinkleSprinkles}
-                alt="Periwinkle Sprinkles"
-                className="sprinkle-bg sprinkle-peri"
-              />
-              <img
-                src={topRatedImage2}
-                className="square-img"
-                alt="top rated nostalgia 2"
-              />
+            <div className="top-rated-item">
+              <div className="top-rated-image-wrapper">
+                <img src={periwinkleSprinkles} alt="Periwinkle Sprinkles" className="sprinkle-bg sprinkle-peri" />
+                <img src={topRatedImage2} className="square-img" alt="top rated nostalgia 2" />
+              </div>
+              <span>Pac-Man</span>
+              <NavLink to="/products" className="shop-now-button">
+                <button>Shop Now</button>
+              </NavLink>
             </div>
-            <span>Pac-Man</span>
-            <NavLink to="/products" className="shop-now-button">
-              <button>Shop Now</button>
-            </NavLink>
-          </div>
 
-          <div className="top-rated-item">
-            <div className="top-rated-image-wrapper">
-              <img
-                src={periwinkleSprinkles}
-                alt="Periwinkle Sprinkles"
-                className="sprinkle-bg sprinkle-peri"
-              />
-              <img
-                src={topRatedImage3}
-                className="square-img"
-                alt="top rated nostalgia 3"
-              />
+            <div className="top-rated-item">
+              <div className="top-rated-image-wrapper">
+                <img src={periwinkleSprinkles} alt="Periwinkle Sprinkles" className="sprinkle-bg sprinkle-peri" />
+                <img src={topRatedImage3} className="square-img" alt="top rated nostalgia 3" />
+              </div>
+              <span>Gameboy Original</span>
+              <NavLink to="/products" className="shop-now-button">
+                <button>Shop Now</button>
+              </NavLink>
             </div>
-            <span>Gameboy Original</span>
-            <NavLink to="/products" className="shop-now-button">
-              <button>Shop Now</button>
-            </NavLink>
           </div>
         </div>
+        <img src={bottomBackground} className="bottom-background" alt="bottom background" />
       </div>
-      <img
-        src={bottomBackground}
-        className="bottom-background"
-        alt="bottom background"
-      />
     </div>
   );
 }
