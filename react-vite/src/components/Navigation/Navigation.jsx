1f44c2602f33de05f1ab917293c03cbe82f7d702
andrea17463 <163758451+andrea17463@users.noreply.github.com>
2025-04-17 19:07:41 -0700

    added more images for products that have less than four images. revised Navigation.jsx and Navigation.css (#124)

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -20,38 +20,40 @@ function Navigation() {
         />
       </div>
 
-      <div className="nav-link">
-        <NavLink to="/" className="home-button">
-          <button>home</button>
-        </NavLink>
-      </div>
-      <div className="nav-link">
-        <NavLink to="/products" className="decade-button">
-          <button>all nostalgia</button>
-        </NavLink>
-      </div>
-      <div className="nav-link">
-        <NavLink to="/products?decade=80s" className="decade-button">
-          <button>80s</button>
-        </NavLink>
-      </div>
-      <div className="nav-link">
-        <NavLink to="/products?decade=90s" className="decade-button">
-          <button>90s</button>
-        </NavLink>
-      </div>
-      <div className="nav-link">
-        <NavLink to="/products?decade=00s" className="decade-button">
-          <button>00s</button>
-        </NavLink>
-      </div>
-      <div className="nav-link cart-container">
-        <NavLink to="/cart" className="cart-link">
-          <div className="cart-wrapper">
-            <FaShoppingBag size={24} />
-            {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
-          </div>
-        </NavLink>
+      <div className="nav-links-container">
+        <div className="nav-link">
+          <NavLink to="/" className="home-button">
+            <button>home</button>
+          </NavLink>
+        </div>
+        <div className="nav-link">
+          <NavLink to="/products" className="decade-button">
+            <button>all nostalgia</button>
+          </NavLink>
+        </div>
+        <div className="nav-link">
+          <NavLink to="/products?decade=80s" className="decade-button">
+            <button>80s</button>
+          </NavLink>
+        </div>
+        <div className="nav-link">
+          <NavLink to="/products?decade=90s" className="decade-button">
+            <button>90s</button>
+          </NavLink>
+        </div>
+        <div className="nav-link">
+          <NavLink to="/products?decade=00s" className="decade-button">
+            <button>00s</button>
+          </NavLink>
+        </div>
+        <div className="nav-link cart-container">
+          <NavLink to="/cart" className="cart-link">
+            <div className="cart-wrapper">
+              <FaShoppingBag size={24} />
+              {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
+            </div>
+          </NavLink>
+        </div>
       </div>
 
       <div className="profile-button">
