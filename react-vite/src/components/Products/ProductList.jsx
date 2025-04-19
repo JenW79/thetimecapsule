d02c0a7bcc276d097010952a1bd9d6dd4cf970ff
JenW79 <jkwill646@gmail.com>
2025-04-18 19:46:58 -0500

    added cards to products to make them stand out from background

--- react-vite/src/components/Products/ProductList.jsx
+++ react-vite/src/components/Products/ProductList.jsx
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../../redux/products';
-import ProductItem from './ProductItem';
-import { useLocation } from 'react-router-dom';
-import './Products.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../redux/products";
+import ProductItem from "./ProductItem";
+import { useLocation } from "react-router-dom";
+import "./Products.css";
 
 const ProductList = () => {
   const dispatch = useDispatch();
-  const productsObj = useSelector(state => state.products);
+  const productsObj = useSelector((state) => state.products);
   const products = Object.values(productsObj);
   const location = useLocation();
 
   const queryParams = new URLSearchParams(location.search);
-  const decade = queryParams.get('decade');
-  const selectedCategory = queryParams.get('category');
+  const decade = queryParams.get("decade");
+  const selectedCategory = queryParams.get("category");
 
   // useEffect(() => {
   //   dispatch(fetchProducts());
@@ -21,14 +21,14 @@ const ProductList = () => {
 
   useEffect(() => {
     let query = "";
-  
+
     if (decade || selectedCategory) {
       const params = new URLSearchParams();
       if (decade) params.append("decade", decade);
       if (selectedCategory) params.append("category", selectedCategory);
       query = "?" + params.toString();
     }
-  
+
     dispatch(fetchProducts(query));
   }, [dispatch, decade, selectedCategory]);
 
@@ -40,8 +40,7 @@ const ProductList = () => {
 
   // if (selectedCategory) {
   //   filteredProducts = filteredProducts.filter(product => product.category === selectedCategory);
-  // }  already filtering from backend. 
-
+  // }  already filtering from backend.
 
   // // hide products owned
   // const sessionUser = useSelector((state) => state.session.user);
@@ -49,11 +48,11 @@ const ProductList = () => {
   //   filteredProducts = filteredProducts.filter(
   //     (product) => product.owner?.id !== sessionUser.id
   //   );
-  // } 
+  // }
 
-  // //hide products owned ^  
+  // //hide products owned ^
   // doing this prevents products from being shown, making it look
-  // like the products dont exist at all. User 1 owns all 3 80's products in the seed. 
+  // like the products dont exist at all. User 1 owns all 3 80's products in the seed.
 
   // if (!filteredProducts.length) {
   //   return <div className="loading">Loading products...</div>;
@@ -61,17 +60,15 @@ const ProductList = () => {
 
   if (!filteredProducts.length) {
     return (
-      <div className="empty-message">
-        No products found in this category.
-      </div>
+      <div className="empty-message">No products found in this category.</div>
     );
-  }  // can keep this or change to your preference. 
+  } // can keep this or change to your preference.
 
   return (
-    <div className="product-list">
+    <div className="products-page-container">
       <h1>
-        {decade ? `${decade} Products` : 'All Products'}
-        {selectedCategory ? ` - ${selectedCategory}` : ''}
+        {decade ? `${decade} Products` : "All Products"}
+        {selectedCategory ? ` - ${selectedCategory}` : ""}
       </h1>
       <div className="products-grid">
         {filteredProducts.map(product => (
