f3d3c77eb554f900b389928dffd629b4a5e69065
JenW79 <jkwill646@gmail.com>
2025-04-18 20:28:32 -0500

    fixed favorites, reviews titles, made favorites a little more user friendly

--- react-vite/src/components/Favorites/FavoritesList.jsx
+++ react-vite/src/components/Favorites/FavoritesList.jsx
@@ -1,34 +1,48 @@
-import { useEffect } from 'react'; 
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchFavorites } from '../../redux/favorites';
-import ProductItem from '../Products/ProductItem';
-import './Favorites.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchFavorites } from "../../redux/favorites";
+import ProductItem from "../Products/ProductItem";
+import "./Favorites.css";
 
 const FavoritesList = () => {
   const dispatch = useDispatch();
-  const favoritesObj = useSelector(state => state.favorites);
+  const favoritesObj = useSelector((state) => state.favorites);
   const favorites = Object.values(favoritesObj);
-  
+
   useEffect(() => {
     dispatch(fetchFavorites());
   }, [dispatch]);
 
-  if (!favorites || !favorites.length) {
+  if (!favorites) {
     return <div className="loading">Loading favorites...</div>;
   }
 
+  if (favorites.length === 0) {
+    return (
+      <div className="no-favorites-message">
+        <h2>You haven’t favorited anything yet 💔</h2>
+        <p>Head over to the products page and show some love!</p>
+        <Link to="/products">
+          <button className="ctaButtonSmall">Browse Products</button>
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className="favorites-section">
-      <h2 className="favorites-title">Your Favorite Products</h2>
+      <h1 className="favorites-title">Your Favorite Products</h1>
       {favorites.length === 0 ? (
-        <p className="no-favorites-message">You haven’t added any favorites yet.</p>
+        <p className="no-favorites-message">
+          You haven’t added any favorites yet.
+        </p>
       ) : (
         <div className="favorites-grid">
-          {favorites.map(favorite => (
-            <ProductItem 
-              key={favorite.id} 
+          {favorites.map((favorite) => (
+            <ProductItem
+              key={favorite.id}
               product={favorite.product}
-              isFavorite={true}  
+              isFavorite={true}
             />
           ))}
         </div>
